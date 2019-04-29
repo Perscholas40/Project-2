60677129b2e5e6db634cf2a14c106eb2433cb5d2
Perscholas40 <todd_wilcher@PSADMINs-MacBook-Air-49.local>
2019-04-29 14:51:41 -0400

    db created

---
+++ db/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require ('mongoose');
+
+
+
+
+
+
+
+
+
+
+module.exports = mongoose
--- server.js
+++ server.js
@@ -1,12 +1,54 @@
 const express = require ('express');
 const path = require('path');
 const app = express();
+const expbs = require('express-handlebars')
 const methodOverride = require('method-override');
 const routes = require('./routes/index')
 
+app.engine('handlebars', expbs({ 
+  defaultLayout: 'main', 
+  layoutsDir: 'views/layouts'
+}));
+
+
 app.set('view engine', 'hbs');
 
-app.use(express.static(path.join(__dirname, '/Public')));
+// Routing
+
+// Landing Page
+app.get('/', (req,res) => {
+  res.render('landing')
+});
+
+// Register Page
+app.get('/register', (req,res) => {
+  res.render('register')
+});
+
+// Donate Page
+app.get('/donate', (req,res) => {
+  res.render('donate')
+});
+
+// Brass Instruments Page
+app.get('/brass', (req,res) => {
+  res.render('brass')
+});
+
+// Woodwind Instruments Page
+app.get('/woodwinds', (req,res) => {
+  res.render('woodwinds')
+});
+
+// Percussion Page
+app.get('/percussion', (req,res) => {
+  res.render('percussion')
+});
+
+// Static Folder
+app.use(express.static(path.join(__dirname, 'Public')));
+
+
 app.use('/', routes); 
 app.use(methodOverride('_method'));
 
