62b03a50e797f95cc08d1c6a5ee7400f6de3f7a7
Perscholas40 <todd_wilcher@PSADMINs-MacBook-Air-49.local>
2019-05-02 01:18:45 -0400

    connection update

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,25 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-const dbConnection = process.env.MONGODB_URI || 'mongodb://localhost:27017/Project-2'
-mongoose.connect(dbConnection, { useNewUrlParser: true})
- .then(() => {
-  console.log("mongo is working");
- })
+// const dbConnection = process.env.MONGODB_URI || 'mongodb://localhost:27017/Project-2'
+// mongoose.connect(dbConnection, { useNewUrlParser: true})
+//  .then(() => {
+//   console.log("mongo is working");
+//  })
 
 
-module.exports = mongoose;
+// module.exports = mongoose;
+
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+  }
+  else {
+    mongoose.connect('mongodb://localhost/express-movies');
+  }
+  mongoose.connection.on('error', function(err) {
+    console.error('MongoDB connection error: ' + err);
+    process.exit(-1);
+    }
+  );
+  mongoose.connection.once('open', function() {
+    console.log("Mongoose has connected to MongoDB!");
+  });
